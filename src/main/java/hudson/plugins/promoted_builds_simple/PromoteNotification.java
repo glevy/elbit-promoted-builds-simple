9f3cff395a4999e2252aaf9fc27c4048666b576e
Gilad Levy <ynot.gilad@gmail.com>
2020-05-21 14:19:58 +0300

    update email body

--- src/main/java/hudson/plugins/promoted_builds_simple/PromoteNotification.java
+++ src/main/java/hudson/plugins/promoted_builds_simple/PromoteNotification.java
@@ -2,8 +2,12 @@ package hudson.plugins.promoted_builds_simple;
 
 import javax.mail.*;
 import javax.mail.internet.*;
+
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.acegisecurity.AccessDeniedException;
 
@@ -11,7 +15,6 @@ import hudson.model.Job;
 import hudson.model.ParameterValue;
 import hudson.model.ParametersAction;
 import hudson.tasks.Mailer;
-
 import jenkins.model.Jenkins;
 
 /**
@@ -29,16 +32,17 @@ public class PromoteNotification {
     public PromoteNotification(String emailList, String subject, String body, String level) {
         this.emailList = emailList;
         this.subject = subject;
-        this.body = body;
+        setBody(body);
     }
 
     public PromoteNotification() {
     }
 
     public String getEmailList() {
-        if (emailList != null)
-            return emailList;
-            else return "";
+        if (this.emailList != null)
+            return this.emailList;
+        else
+            return "";
     }
 
     public void setEmailList(String emailList) {
@@ -46,9 +50,10 @@ public class PromoteNotification {
     }
 
     public String getSubject() {
-        if (subject != null)
-           return subject;
-            else return "";
+        if (this.subject != null)
+            return this.subject;
+        else
+            return "";
     }
 
     public void setSubject(String subject) {
@@ -56,36 +61,48 @@ public class PromoteNotification {
     }
 
     public String getBody() {
-        if (body != null)
-            return body;
-            else return "";
+        if (this.body != null)
+            return this.body;
+        else
+            return "";
     }
 
     public void setBody(String body) {
-        this.body = body;
+         if (body != null) {
+             this.body = body;
+        }
     }
 
     public String getLevel() {
         if (level != null)
             return level;
-            else return "";
+        else
+            return "";
     }
 
     public void setLevel(String level) {
         this.level = level;
     }
 
-  
     public void notify(String buildURL, String buildName, Integer buildNum, String level) {
 
         setLevel(level);
-            try {
-            ParametersAction pa = Jenkins.getInstance().getItemByFullName(buildName, Job.class).getBuildByNumber(buildNum).getAction(ParametersAction.class);
-            if (pa != null){
+        try {
+
+            Map<String, String> env = Jenkins.getInstance().getItemByFullName(buildName, Job.class)
+                    .getBuildByNumber(buildNum).getEnvironment( );
+            System.out.println(env);
+            // static EnvActionImpl env
+            // forRun(Jenkins.getInstance().getItemByFullName(buildName,
+            // Job.class).getBuildByNumber(buildNum));
+            ParametersAction pa = Jenkins.getInstance().getItemByFullName(buildName, Job.class)
+                    .getBuildByNumber(buildNum).getAction(ParametersAction.class);
+            if (pa != null) {
                 List<ParameterValue> params = pa.getParameters();
-                if(params.size() > 0){
+                if (params.size() > 0) {
                     for (ParameterValue p : params) {
-                        if (p == null) continue;
+                        if (p == null)
+                            continue;
                         System.out.println(p.getName());
                         if (p.getName().equals("Promotion notification list")) {
                             setEmailList(String.valueOf(p.getValue()));
@@ -96,29 +113,46 @@ public class PromoteNotification {
                             System.out.println(getSubject());
                         }
                         if (p.getName().equals("Promotion notification Body")) {
-                            setBody(String.valueOf(p.getValue()));
-                            System.out.println(getBody());
+                            String emailBody = String.valueOf(p.getValue());
+                            if (emailBody != null) {
+                                emailBody = emailBody.replaceAll(Pattern.quote("!!BUILD_NUMBER"), String.valueOf(buildNum));
+                                emailBody = emailBody.replaceAll(Pattern.quote("!!JOB_NAME"), buildName);
+                                emailBody = emailBody.replaceAll(Pattern.quote("!!BUILD_URL"), buildURL);
+                                emailBody = emailBody.replaceAll(Pattern.quote("!!PROMOTION_LVL"), this.level);
+                                setBody(emailBody);
+                                System.out.println(getBody());
+                            }
                         }
-                    } 
+                    }
                 }
             }
-            
-            if (!getEmailList().isEmpty()){
-                if (getSubject().isEmpty()){
-                    setSubject(
-                            "Build Promotion Notification: " + buildName + ":" + buildNum + " was promoted to " + this.level);
+
+            if (!getEmailList().isEmpty()) {
+                if (getSubject().isEmpty()) {
+                    setSubject("Build Promotion Notification: " + buildName + ":" + buildNum + " was promoted to "
+                            + this.level);
                 }
-                if (getBody().isEmpty()){
-                    setBody("You are getting this message because you were added to this Jenkins build promotion notification list by the job owner.\n\n "
-                            + buildName + " build number " + buildNum + " was promoted to level: " + level + "\n\n Build URL: "
-                            + buildURL);
+                if (getBody().isEmpty()) {
+                    setBody("<div><p>You are getting this message because you were added to this Jenkins build promotion notification list by the job owner.</p><p><strong>"
+                            + buildName + "</strong> build number <strong>" + buildNum
+                            + "</strong> was promoted to level: <strong>" + level + "</strong></p><p>Build URL: " + buildURL
+                            + "</p></div>");
+
+                    // setBody("You are getting this message because you were added to this Jenkins
+                    // build promotion notification list by the job owner.\n\n "
+                    // + buildName + " build number " + buildNum + " was promoted to level: " +
+                    // level + "\n\n Build URL: "
+                    // + buildURL);
                 }
 
                 System.out.println(subject);
 
                 sendMail();
-            } else{ System.out.println("Promotion notification list is empty.\nPlease make sure a job string parameter by this name exists and is not empty.");}
-        } catch (AccessDeniedException e) {
+            } else {
+                System.out.println(
+                        "Promotion notification list is empty.\nPlease make sure a job string parameter by this name exists and is not empty.");
+            }
+        } catch (AccessDeniedException | IOException | InterruptedException e) {
           e.printStackTrace();
       }
           
@@ -132,12 +166,21 @@ public class PromoteNotification {
                 InternetAddress sender = new InternetAddress(Mailer.descriptor().getAdminAddress());
                 String rt = String.valueOf(sender);
                 MimeMessage msg = new MimeMessage(Mailer.descriptor().createSession());
-                msg.setSubject(this.subject);
-                msg.setText(this.body, "utf-8");
+                msg.setSubject(getSubject());
+                Pattern htmlPattern = Pattern.compile(".*\\<[^>]+>.*", Pattern.DOTALL);
+                boolean isHTML = htmlPattern.matcher(getBody()).matches();
+                if(isHTML){
+                    System.out.println("HTML body was identified");
+                    msg.setContent(getBody(), "text/html");
+                }
+                else{
+                    System.out.println("text body was identified");
+                    msg.setText(getBody(), "utf-8");
+                }
                 msg.setFrom(sender);
                 msg.setReplyTo(InternetAddress.parse(rt, false));
                 msg.setSentDate(new Date());
-                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(this.emailList, false));
+                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(getEmailList(), false));
                 Transport.send(msg);     
 
             System.out.println("EMail Sent Successfully!!");
